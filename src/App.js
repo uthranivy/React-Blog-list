dbbb2da16618657062dbd5cc76fe808b6b4ce31a
Uthra G <uthra.156@gmail.com>
2024-02-07 19:12:35 +0530

    first commit blog

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./index.css";
+import { data } from "./data";
+import Loading from "./Loading";
+import { BsCalendar3, BsCardText, BsPersonCircle } from "react-icons/bs";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [value, setValue] = useState(0);
+  const [post, setPost] = React.useState([]);
+
+  const fetchPost = async () => {
+    setLoading(true);
+    try {
+      const posts = data;
+      setLoading(false);
+      setPost(posts);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchPost();
+  }, []);
+
+  if (loading) {
+    return (
+      <div>
+        <Loading />
+      </div>
+    );
+  }
+
+  const { shortpost, dates, person, blogpost } = post[value];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="blog-container">
+        <nav className="nav-blog">
+          {post.map((item, index) => {
+            return (
+              <button
+                type="button"
+                key={item.order}
+                onClick={() => setValue(index)}
+                className={`nav-btn-main ${index === value && "active-tab"}`}
+              >
+                {item.title}
+              </button>
+            );
+          })}
+        </nav>
+        <article className="nav-main">
+          <section className="blog-section">
+            <h3 className="blog-head">{shortpost}</h3>
+            <div className="flex-item">
+              <p>
+                <BsCalendar3 /> <small> {dates}</small>
+              </p>
+              <h6 className="author">
+                <BsPersonCircle /> {person}
+              </h6>
+            </div>
+          </section>
+
+          <ul className="list">
+            {blogpost.map((postslist, index) => {
+              return (
+                <li className="blog-list" key={index}>
+                  <span>
+                    <BsCardText className="list-icon" />{" "}
+                  </span>
+                  <span className="list-content">{postslist}</span>
+                </li>
+              );
+            })}
+          </ul>
+        </article>
+      </div>
     </div>
   );
+
+  // return (
+  //   <div>
+  //     {data.map((blogposts) => {
+  //         return <Blogs key={blogposts.id} {...blogposts}></Blogs>;
+  //       })}
+  //   </div>
+  // )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,43 @@
+
+
+export const data = [
+    {
+      "id": "1",
+      "order": 1,
+      "title": "World",
+      "shortpost": "Two ships, one headed to India, attacked in Red Sea by Iran-backed Houthi rebels",
+      "dates": "03 December 2023", 
+      "blogpost": [
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde recusandae animi, incidunt soluta eaque officia consectetur laboriosam ea quo aspernatur, atque corporis numquam delectus nostrum. Voluptas, tempore recusandae!",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde recusandae animi, atque corporis numquam delectus nostrum. Voluptas, tempore recusandae!",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde recusandae animi, incidunt soluta eaque officia consectetur laboriosam ea quo aspernatur, tempore recusandae!"
+      ],
+      "person": "Mark Joe"
+    },
+    {
+      "id": "2",
+      "order": 2,
+      "title": "Sports",
+      "shortpost": "Jake Fraser-McGurk reminds me of young David Warner: Ricky Ponting",
+      "dates": "23 December 2023",
+      "blogpost": [
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde recusandae animi, incidunt soluta eaque officia consectetur laboriosam ea quo aspernatur, atque corporis numquam delectus nostrum. Voluptas, tempore recusandae!",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde recusandae animi, atque corporis numquam delectus nostrum. Voluptas, tempore recusandae!",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde recusandae animi, incidunt soluta eaque officia consectetur laboriosam ea quo aspernatur, tempore recusandae!"
+      ],
+      "person": "Arun Kumar"
+    },
+    {
+      "id": "3",
+      "order": 3,
+      "title": "Technology",
+      "shortpost": "Safer Internet Day 2024: 8 iPhone features that keep you safe online ",
+      "dates": "28 December 2023",
+      "blogpost": [
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde recusandae animi, incidunt soluta eaque officia consectetur laboriosam ea quo aspernatur, atque corporis numquam delectus nostrum. Voluptas, tempore recusandae!",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde recusandae animi, atque corporis numquam delectus nostrum. Voluptas, tempore recusandae!",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde recusandae animi, incidunt soluta eaque officia consectetur laboriosam ea quo aspernatur, tempore recusandae!"
+      ],
+      "person": "Mahi Raj"
+    }
+  ]
